f691ff22f0efe1ee68acafed627284b96882b006
Harouna Traore <h.traore@icloud.com>
2017-11-24 20:45:19 +0100

    add network ip configuration

--- network.lua
+++ network.lua
@@ -4,16 +4,20 @@ function network.setup(ssid, pwd)
   wifi.setphymode(wifi.PHYMODE_G)
   wifi.setmode(wifi.SOFTAP)
 
-  config={
-    ssid=ssid
-    pwd=pwd
-    auth=wifi.WPA2_PSK
-    max=1
+  config = {
+    ssid = ssid,
+    pwd = pwd,
+    auth = wifi.WPA2_PSK,
+    max = 1,
+    ip = "192.168.1.1",
+    netmask = "255.255.255.0",
+    gateway = "192.168.1.1"
   }
   wifi.ap.config(config)
+  wifi.ap.setip(config)
 
-  dhcp_config={
-    start="192.168.1.100"
+  dhcp_config = {
+    start = "192.168.1.100"
   }
   wifi.ap.dhcp.config(dhcp_config)
   wifi.ap.dhcp.start()
