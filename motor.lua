d1b75dcfcf94af32d6161ac8ca343d193947e8ee
Harouna Traore <h.traore@icloud.com>
2017-11-24 20:45:53 +0100

    add motor functions

--- motor.lua
+++ motor.lua
@@ -1,7 +1,15 @@
 local motor = {}
 
 function motor.setup(enable, input1, input2)
-  -- setup the motor
+end
+
+function motor.on(enable, input1, input2, power)
+end
+
+function motor.stop(enable, input1, input2)
+end
+
+function motor.neutral(enable)
 end
 
 return motor
